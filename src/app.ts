9d3025559ad9d7e1a0ee57ca1c4b28aa9c67196b
Alexandr Kolesov <alexandr.kolesov@dsr-corporation.com>
2021-02-19 11:25:29 +0300

    Fixed port env name

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,6 @@ const server = new Server();
 server.app.use('/api', server.router);
 
 // make server listen on some port
-((port = process.env.APP_PORT || 5000) => {
+((port = process.env.PORT || 5000) => {
   server.app.listen(port, () => console.log(`> Listening on port ${port}`));
 })();
