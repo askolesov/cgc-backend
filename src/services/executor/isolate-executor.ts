1a7edc362a7a50b82f64ec26842293cb23d967e1
Alexandr Kolesov <alexandr.kolesov@dsr-corporation.com>
2021-03-14 15:55:29 +0300

    Initial version of executor

--- src/services/executor/isolate-executor.ts
+++ src/services/executor/isolate-executor.ts
@@ -1,32 +1,33 @@
 import { Isolate } from 'isolated-vm';
-import { IExecutor } from './iexecutor';
 
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call */
-export class IsolateExecutor implements IExecutor {
+export class IsolateExecutor {
     private ivm: any;
 
-    constructor(private sourceCode: string, memoryLimitMb = 32) {
+    constructor(
+        private sourceCode: string,
+        private entryPoint: string,
+        memoryLimitMb = 32,
+        private timeLimitMs: number = 2000,
+    ) {
         this.ivm = new Isolate({ memoryLimit: memoryLimitMb });
     }
 
-    execute<T>(functionName: string, ...arguments_: any[]): T {
+    execute<T>(...arguments_: any[]): T {
         // Creating context
         const context = this.ivm.createContextSync();
 
-        // Evaluating source code
-        context.evalSync(this.sourceCode);
-
-        // Passing arguments: functions as references, other values by copying
+        // Passing arguments: functions as references, other parameters by value
         const jail = context.global;
-        jail.setSync('global', jail.derefInto()); // Making global object accessible globally ;)
+        jail.setSync('global', jail.derefInto()); // Making global object accessible
         context.evalClosureSync(`global.args = [];`);
 
         for (const argument of arguments_) {
             if (typeof argument === 'function') {
                 context.evalClosureSync(
                     `args.push(function(...args) {
-                    $0.applySync(undefined, args, { arguments: { copy: true } });
+                    return $0.applySync(undefined, args, { arguments: { copy: true }, result: { copy: true } });
                 })`,
                     [argument],
                     { arguments: { reference: true } },
@@ -37,13 +38,18 @@ export class IsolateExecutor implements IExecutor {
         }
 
         // Calling the entry point with passed args
-        const result = context.evalClosureSync(`return ${functionName}(...args);`, [], {
-            result: { copy: true },
+        const result = context.evalSync(`${this.sourceCode}; ${this.entryPoint}(...args)`, {
+            timeout: this.timeLimitMs,
+            copy: true,
         });
 
         // Returning result
         context.release();
         return result.result as T;
+    }
+
+    closure<T>(): (...arguments_: any[]) => T {
+        return (...arguments_: any[]) => this.execute<T>(...arguments_);
     }
 }
 /* eslint-enable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access */
--- src/services/executors/iexecutor.ts
+++
@@ -1,4 +0,0 @@
-export interface IExecutor {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    execute<T>(functionName: string, ...arguments_: any[]): T;
-}
